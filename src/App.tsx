dcd7ce19fa915d01ffcff8ff22a8c3ff8d95db33
Priyanshu Uchat <priyanshu.uc507@gmail.com>
2025-12-04 23:42:38 +0530

    fix: use HashRouter to prevent 404 on page reloads for static hosting

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -70,7 +70,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <div className="min-h-screen bg-background">
             <Navbar cartItemCount={cartItemCount} onCartOpen={() => setCartOpen(true)} />
             
@@ -112,7 +112,7 @@ const App = () => {
 
             <WhatsAppButton />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
